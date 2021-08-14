3800eddb596236dcc14ea13e78c77abc549937ec
伊欧 <langyo.china@gmail.com>
2021-08-14 21:57:53 +0800

    🎨 将轨道的存储方式从线性表改为了散列表。

--- type.d.ts
+++ type.d.ts
@@ -1,93 +1,94 @@
 declare namespace CoronaBGMEditorConfig {
   interface IMain {
     musicFiles: {
-      [name: string]: string
-    },
+      [name: string]: string;
+    };
     unitWeight: {
-      "AlliedScoutInfantry": number,
-      "AlliedAntiInfantryInfantry": number,
-      "AlliedAntiVehicleInfantry": number,
-      "AlliedEngineer": number,
-      "AlliedInfiltrationInfantry": number,
-      "AlliedCommandoTech1": number,
-      "AlliedMiner": number,
-      "AlliedAntiInfantryVehicle": number,
-      "AlliedAntiAirVehicleTech1": number,
-      "AlliedAntiVehicleVehicleTech1": number,
-      "AlliedAntiStructureVehicle": number,
-      "AlliedAntiVehicleVehicleTech3": number,
-      "AlliedMCV": number,
-      "AlliedAntiGroundAircraft": number,
-      "AlliedFighterAircraft": number,
-      "AlliedSupportAircraft": number,
-      "AlliedBomberAircraft": number,
-      "AlliedSupersonicBomber": number,
-      "AlliedAntiNavalScout": number,
-      "AlliedAntiAirShip": number,
-      "AlliedAntiNavyShipTech1": number,
-      "AlliedAntiStructureShip": number,
-      "AlliedConstructionYard": number,
-      "AlliedOutPost": number,
-      "AlliedPowerPlant": number,
-      "AlliedBarracks": 500,
-      "AlliedRefinery": number,
-      "AlliedWarFactory": number,
-      "AlliedNavalYard": number,
-      "AlliedAirfield": number,
-      "AlliedTechStructure": number,
-      "AlliedWallPiece": number,
-      "AlliedWallSegmentPiece": number,
-      "AlliedBaseDefense": number,
-      "AlliedBaseDefenseAdvanced": number,
-      "AlliedSuperWeapon": number,
-      "AlliedSuperWeaponAdvanced": number
-    },
+      AlliedScoutInfantry: number;
+      AlliedAntiInfantryInfantry: number;
+      AlliedAntiVehicleInfantry: number;
+      AlliedEngineer: number;
+      AlliedInfiltrationInfantry: number;
+      AlliedCommandoTech1: number;
+      AlliedMiner: number;
+      AlliedAntiInfantryVehicle: number;
+      AlliedAntiAirVehicleTech1: number;
+      AlliedAntiVehicleVehicleTech1: number;
+      AlliedAntiStructureVehicle: number;
+      AlliedAntiVehicleVehicleTech3: number;
+      AlliedMCV: number;
+      AlliedAntiGroundAircraft: number;
+      AlliedFighterAircraft: number;
+      AlliedSupportAircraft: number;
+      AlliedBomberAircraft: number;
+      AlliedSupersonicBomber: number;
+      AlliedAntiNavalScout: number;
+      AlliedAntiAirShip: number;
+      AlliedAntiNavyShipTech1: number;
+      AlliedAntiStructureShip: number;
+      AlliedConstructionYard: number;
+      AlliedOutPost: number;
+      AlliedPowerPlant: number;
+      AlliedBarracks: 500;
+      AlliedRefinery: number;
+      AlliedWarFactory: number;
+      AlliedNavalYard: number;
+      AlliedAirfield: number;
+      AlliedTechStructure: number;
+      AlliedWallPiece: number;
+      AlliedWallSegmentPiece: number;
+      AlliedBaseDefense: number;
+      AlliedBaseDefenseAdvanced: number;
+      AlliedSuperWeapon: number;
+      AlliedSuperWeaponAdvanced: number;
+    };
     fsmConfig: {
-      interval: number,
-      fightThreshold: number,
-      advantageThreshold: number,
-      disadvantageThreshold: number
-    }
-    tracks: ITrack[]
+      interval: number;
+      fightThreshold: number;
+      advantageThreshold: number;
+      disadvantageThreshold: number;
+    };
+    tracks: ITrack[];
   }
 
   interface ITrack {
-    id: string,
-    musicId: string,
-    startOffset: number,
-    length: number,
-    beatsPerMinutes: number,
-    beatsPerBar: number,
-    checkPoints?: ICheckPoint[],
-    defaultCheckPoints?: IDefaultCheckPoint[]
+    id: string;
+    order: number;
+    musicId: string;
+    startOffset: number;
+    length: number;
+    beatsPerMinutes: number;
+    beatsPerBar: number;
+    checkPoints?: ICheckPoint[];
+    defaultCheckPoints?: IDefaultCheckPoint[];
   }
 
   interface ICheckPoint {
-    time: number,
-    destinations?: IDestination[]
-    defaultDestinations?: IDefaultDestination[]
+    time: number;
+    destinations?: IDestination[];
+    defaultDestinations?: IDefaultDestination[];
   }
 
   interface IDefaultCheckPoint {
-    condition: string,
-    jumpTo: IJumpTo[]
+    condition: string;
+    jumpTo: IJumpTo[];
   }
 
   interface IDestination {
-    condition: string,
-    jumpTo: IJumpTo[]
+    condition: string;
+    jumpTo: IJumpTo[];
   }
 
   interface IDefaultDestination {
-    jumpTo: IJumpTo[]
+    jumpTo: IJumpTo[];
   }
 
   interface IJumpTo {
-    targetTrackId: string,
-    targetOffset: number,
-    fadeOutDelay: number,
-    fadeOutDuration: number,
-    targetFadeInDelay: number,
-    targetFadeInDuration: number
+    targetTrackId: string;
+    targetOffset: number;
+    fadeOutDelay: number;
+    fadeOutDuration: number;
+    targetFadeInDelay: number;
+    targetFadeInDuration: number;
   }
 }
